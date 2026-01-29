b04e6a984da0391f2f6725f5b1817398805c7b41
nichelemkt3-hub <nichelemkt3@gmail.com>
2026-01-29 08:24:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let tentativas = 3;
+
+function chutar() {
+  if (tentativas <= 0) {
+    alert("Fim de jogo!");
+    return;
+  }
+
+  tentativas--;
+  document.getElementById("tentativas").innerText = tentativas;
+
+  const ganhou = Math.random() < 0.3;
+
+  if (ganhou) {
+    document.getElementById("resultado").innerText =
+      "GOOOOL! Você ganhou um cupom!";
+  } else {
+    document.getElementById("resultado").innerText =
+      "Defesa! Tente de novo.";
+  }
+}
 
